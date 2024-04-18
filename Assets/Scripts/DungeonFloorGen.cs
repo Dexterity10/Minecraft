4c61616e2e78601d57409c840c76574bc05b4b6a
Dexterity10 <alex.goemans@gmail.com>
2024-04-18 12:14:08 -1000

    Dungeon Generator v1
    + simple rooms
    + hallways
    To fix:
    - z fighting with floor tiles
    - more ceilings
    To add:
    - more rooms
    - more junctions
    - y-axis rooms?

--- Assets/Scripts/DungeonFloorGen.cs
+++ Assets/Scripts/DungeonFloorGen.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class DungeonFloorGen : MonoBehaviour
 {
     public Vector3Int startPos = new Vector3Int(0, 0, 0);
+    GameObject chosenRoom;
     public GameObject floor;
-    public GameObject wall;
+    public GameObject[] room;
+    public GameObject[] hall;
     //public GameObject[] roomlist = {
     //    none,
     //    simple
@@ -16,10 +18,35 @@ public class DungeonFloorGen : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int chooseRoom = Random.Range(0, 2);
-        int chooseBlock = Random.Range(0, 2);
-        makeRoom();
 
+        for (int b = 0; b < 8; b++)
+        {
+
+            for (int i = 0; i < 8; i++)
+            {
+
+
+                startPos = new Vector3Int(b * 8, 0, 8 * i);
+                if ((i+b) % 2 == 0)
+                {
+                    
+                    int chooseItem = Random.Range(0, room.Length);
+                    Debug.Log(room.Length);
+                    Debug.Log(chooseItem);
+                    chosenRoom = room[chooseItem];
+
+                }
+                else
+                {
+                    int chooseItem = Random.Range(0, hall.Length);
+                    chosenRoom = hall[chooseItem];
+                }
+                makeRoom(chosenRoom);
+
+            }
+
+
+        }
     }
 
     // Update is called once per frame
@@ -28,9 +55,10 @@ public class DungeonFloorGen : MonoBehaviour
 
     }
 
-    void makeRoom()
+    void makeRoom(GameObject roomtype)
     {
-        
         Instantiate(floor, startPos, floor.transform.rotation);
+        Instantiate(chosenRoom, startPos, chosenRoom.transform.rotation);
+
     }
 }
