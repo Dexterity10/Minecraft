aed0f890df6e850a7b4ddd63c095ee870dece2f1
Dexterity10 <alex.goemans@gmail.com>
2024-05-07 11:21:07 -1000

    DunGen final draft 1
    +Added makeDoor and makeWall functions
    +Increased the dungeon dimensions to 5x16x5 rooms (3200 blocks)

--- Assets/Scripts/DungeonFloorGen.cs
+++ Assets/Scripts/DungeonFloorGen.cs
@@ -53,12 +53,96 @@ public class DungeonFloorGen : MonoBehaviour
 
     void makeRoom(int x, int y, int z, string block)
     {
-        //makeWall(x, y, z, "x");
-        //makeWall(x, y, z, "z");
+        makeWall(x, y, z, block);
+        makeDoor(x, y, z);
 
-        //make doorways
-        //make ceiling
+    }
+    void makeDoor(int x, int y, int z)
+    {
 
+
+        for (int i = 0; i < Random.Range(1, 5); i++)
+        {
+            int doorDir = Random.Range(0, 2); // pick between 0 and 1
+            int doorAbs = Random.Range(0, 2); // pick between 0 and 1
+
+            if (doorDir == 0) // 0 is X, 1 is Z
+            { //X
+                if (doorAbs == 0) // 0 is positive, 1 is negative
+                {
+                    for (int a = 1; a < 8; a++)
+                    {
+                        if (a > 2 && a < 6)
+                        {
+                            world[new Vector3Int(x + a, y + 1, z)] = "air";
+                            world[new Vector3Int(x + a, y + 2, z)] = "air";
+                        }
+                    }
+                    world[new Vector3Int(x + 4, y + 3, z)] = "air";
+                }
+                else
+                {
+                    for (int a = 1; a < 8; a++)
+                    {
+                        if (a > 2 && a < 5)
+                        {
+                            world[new Vector3Int(x + a, y, z + 8)] = "air";
+                            world[new Vector3Int(x + a, y + 1, z + 8)] = "air";
+                        }
+                    }
+                    world[new Vector3Int(x + 4, y + 3, z + 8)] = "air";
+                }
+            }
+            else
+            { //Z
+                if (doorAbs == 0) // 0 is positive, 1 is negative
+                {
+                    for (int c = 1; c < 8; c++)
+                    {
+                        if (c > 2 && c < 6)
+                        {
+                            world[new Vector3Int(x, y + 1, z + c)] = "air";
+                            world[new Vector3Int(x, y + 2, z + c)] = "air";
+                        }
+                    }
+                    world[new Vector3Int(x, y + 3, z + 4)] = "air";
+                }
+                else
+                {
+                    for (int c = 1; c < 8; c++)
+                    {
+                        if (c > 2 && c < 5)
+                        {
+                            world[new Vector3Int(x + 8, y, z + c)] = "air";
+                            world[new Vector3Int(x + 8, y + 1, z + c)] = "air";
+                        }
+                    }
+                    world[new Vector3Int(x + 8, y + 2, z + 4)] = "air";
+                }
+            }
+        }
+    }
+
+
+    void makeWall(int x, int y, int z, string block)
+    {
+        for (int a = 0; a < 8; a++)
+        {
+
+            for (int c = 0; c < 8; c++)
+            {
+                {
+                    for (int b = 0; b < 8; b++)
+                    {
+                        if ((x + a) % 8 == 0 || (z + c) % 8 == 0)
+                        {
+                            world[new Vector3Int(x + a, y + b, z + c)] = block;
+                        }
+                    }
+                }
+            }
+
+        }
     }
     void makeFloor(int x, int y, int z, string block) //also makes ceiling; alternatively, creates stairs
     {
@@ -70,14 +154,6 @@ public class DungeonFloorGen : MonoBehaviour
             {
                 world[new Vector3Int(x + a, y, z + c)] = block;
 
-                // if the coord is touching the corner of the room, make a wall
-                if ((x + a) % 8 == 0 || (z + c) % 8 == 0)
-                {
-                    for (int b = 0; b < 8; b++)
-                    {
-                        world[new Vector3Int(x + a, y + 1, z + c)] = block;
-                    }
-                }
             }
 
         }
@@ -88,29 +164,38 @@ public class DungeonFloorGen : MonoBehaviour
         if (Random.Range(0, floor.Length + 1) == floor.Length)
         {
             int stairDir = Random.Range(0, 2); //pick 0 or 1
-            int stairAbs = Random.Range(1, 3); //pick 1 or 2
-            //make stairs
-            for (int ab = 0; ab < 8; ab++) //can I put an if statement inside a for loop?
+            int stairAbs = Random.Range(0, 2); //pick 0 or 1
+                                               //make stairs
+            for (int ab = 0; ab < 8; ab++)
             {
 
                 for (int c = 0; c < 8; c++)
                 {
-                    //if (stairDir == 0) //pick a random stair direction
-                    //{
-                        world[new Vector3Int(x + ab * ((-1) ^ stairAbs), y + ab, z)] = block;
-                    //}
-                    //else
-                    //{
-                    //    world[new Vector3Int(x, y + ab, z + ab * ((-1) ^ stairAbs))] = block;
-                    //}
+                    if (stairDir == 0) //pick a random stair direction
+                    {
+                        if (stairAbs == 0)
+                        {
+                            world[new Vector3Int(x + ab, y + ab, z + 1)] = block;
+                        }
+                        else world[new Vector3Int(x + 8 - ab, y + ab, z + 1)] = block;
+                    }
+                    else
+                    {
+                        if (stairAbs == 0)
+                        {
+                            world[new Vector3Int(x + 1, y + ab, z + ab)] = block;
+                        }
+                        else world[new Vector3Int(x + 1, y + ab, z + 8 - ab)] = block;
+
+                    }
                 }
 
             }
             //make space for stairs to connect to next floor
-            for (int a = 0; a < 8; a++)
+            for (int a = 1; a < 7; a++)
             {
 
-                for (int c = 0; c < 8; c++)
+                for (int c = 1; c < 7; c++)
                 {
                     world[new Vector3Int(x + a, y + 8, z + c)] = "air";
 
@@ -151,29 +236,4 @@ public class DungeonFloorGen : MonoBehaviour
             }
         }
     }
-    void makeWall(int x, int y, int z, string dir)
-    {
-        if (dir == "x")
-        {
-            for (int b = 0; b < 8; b++)
-            {
-                for (int a = 0; a < 8; a++)
-                {
-                    world[new Vector3Int(x + a, y + b, z)] = "stone";
-                }
-            }
-        }
-
-        else //if dir == "z"
-        {
-            for (int b = 0; b < 8; b++)
-            {
-                for (int c = 0; c < 8; c++)
-                {
-                    world[new Vector3Int(x, y + b, z + c)] = "stone";
-                }
-            }
-        }
-    }
 }
-
